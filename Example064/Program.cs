7890c16dfb2b31a647e22a2c0674934a374a8f6e
Berus Frager <gitmailru@yandex.ru>
2022-09-03 11:22:14 +0300

    Коммит изменений файлов папки проекта Example064

--- Example064/Program.cs
+++ Example064/Program.cs
@@ -10,17 +10,9 @@ Console.Write("Введите значение, до которого нужно
 int maxNumber = int.Parse(Console.ReadLine());
 if (minNumber > 0 && maxNumber > 0 && minNumber <= maxNumber)
 {
-// Переделывает строку в число. Переделывает только в целое число,
-// Convert. со монгоими типами. При этом, если в int.Parse будет
-// ссылка на "null" метод выдаст ошибку, метод Convert. выдаст
-// исключение
 string PrintNumbers(int start, int end)
-// Метод с начальным значением start и конечным end
 {
-    // Следующий случай - базовыЙ, который возвращает число
-    // start, когда оно достигнет end, в строковом виде
     if (start == end) return start.ToString();  
-    // Следующий - рекурсивный случай
     return(start + ", " + PrintNumbers(start + 1, end));    
 }
 Console.WriteLine(PrintNumbers(minNumber, maxNumber));
